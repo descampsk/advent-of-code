c3e01cf29fe0fadb0bb2179fcf4d626217981625
descampsk <22590202+descampsk@users.noreply.github.com>
2023-12-01 08:46:38 +0100

    [descampsk] day 01

---
+++ 2023/descampsk/.eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb-base",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".ts"],
+      },
+    },
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier", "import"],
+  rules: {
+    "no-plusplus": "off",
+    "import/extensions": "off",
+    "no-console": "off",
+    "import/prefer-default-export": "off",
+    "no-restricted-syntax": "off",
+    "import/no-unresolved": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+  },
+};
---
+++ 2023/descampsk/README.md
@@ -0,0 +1,13 @@
+# Avent of code of descampk
+
+## Installation
+
+```bash
+npm i
+```
+
+## Create the day and watch the resolve
+
+```bash
+npm start 1
+```
