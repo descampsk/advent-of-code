81c7134af1806f2157593a3333cb566fb864acf0
descampsk <22590202+descampsk@users.noreply.github.com>
2023-12-01 08:58:22 +0100

    [descampsk] day 1 improvments

--- 2023/descampsk/src/day01/index.ts
+++ 2023/descampsk/src/day01/index.ts
@@ -18,56 +18,17 @@ const testFile2 = join(
 ).replace(/\/dist\//g, "/src/");
 const inputTest2 = readFileSync(testFile2, "utf-8");
 
-const part1 = (rawInput: string) => {
-  const lines = parseInput(rawInput);
-  let total = 0;
-  for (const line of lines) {
-    let firstNumber: number | null = null;
-    let lastNumber: number | null = null;
-    for (const letter of line) {
-      const number = Number.parseInt(letter, 10);
-      if (!Number.isNaN(number)) {
-        if (firstNumber === null) {
-          firstNumber = number;
-        }
-        lastNumber = number;
-      }
-    }
-    total += Number.parseInt(`${firstNumber}${lastNumber}`, 10);
-  }
-  return total;
-};
-
-const digitsToLetters = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
-  "1": 1,
-  "2": 2,
-  "3": 3,
-  "4": 4,
-  "5": 5,
-  "6": 6,
-  "7": 7,
-  "8": 8,
-  "9": 9,
-};
-
-const part2 = (rawInput: string) => {
-  const lines = parseInput(rawInput);
+const computeTotal = (
+  lines: string[],
+  allowedDigits: Record<string, number>,
+) => {
   let total = 0;
   for (const line of lines) {
     let firstNumber: number = 0;
     let firstNumberPosition: number = Infinity;
     let lastNumber = 0;
     let lastNumberPosition = -1;
-    for (const [key, value] of Object.entries(digitsToLetters)) {
+    for (const [key, value] of Object.entries(allowedDigits)) {
       const firstPosition = line.indexOf(key);
       const lastPosition = line.lastIndexOf(key);
       if (firstPosition !== -1 && firstPosition < firstNumberPosition) {
@@ -82,6 +43,45 @@ const part2 = (rawInput: string) => {
     total += Number.parseInt(`${firstNumber}${lastNumber}`, 10);
   }
   return total;
+};
+
+const part1 = (rawInput: string) => {
+  const lines = parseInput(rawInput);
+  return computeTotal(lines, {
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+  });
+};
+
+const part2 = (rawInput: string) => {
+  const lines = parseInput(rawInput);
+  return computeTotal(lines, {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+  });
 };
 
 run({
