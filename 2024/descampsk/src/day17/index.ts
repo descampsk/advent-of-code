86fa0025e763d602c57d2ad4ac5116c34a92d6e9
descampsk <22590202+descampsk@users.noreply.github.com>
2024-12-17 14:15:36 +0100

    [descampsk] 2024 day 17 first commit

---
+++ 2024/descampsk/src/day17/index.ts
@@ -0,0 +1,51 @@
+import run from "aocrunner";
+import { readFileSync } from "fs";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import _ from "lodash";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const parseInput = (rawInput: string) => rawInput.split(/\r?\n/);
+const parseFile = (filename: string) =>
+  readFileSync(
+    join(dirname(fileURLToPath(import.meta.url)), filename).replace(
+      /\/dist\//g,
+      "/src/",
+    ),
+    "utf-8",
+  );
+
+const inputTest = parseFile("input.test.txt");
+
+const part1 = (rawInput: string) => {
+  const lines = parseInput(rawInput);
+  return 0;
+};
+
+const part2 = (rawInput: string) => {
+  const lines = parseInput(rawInput);
+  return 0;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: inputTest,
+        expected: "4,6,3,5,6,3,5,2,1,0",
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: inputTest,
+        expected: "",
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: true,
+});
